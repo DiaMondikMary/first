df1efda69d3fcfcb832192b75e8709fbfac48c20
DiaMondikMary <nambaka.mary@gmail.com>
2024-07-30 09:15:29 +0300

    Add files via upload

--- dz.py
+++ dz.py
@@ -1,2 +1,7 @@
+#Задача 1
 for co,ca,po in zip (['Russia','USA','UK','Germany','France','India'],['Moscow','Washington','London','Berlin','Paris','Delhi'],[i.replace('_','') for i in ['145_934_462','331_002_651','80_345_321','67_886_011','65_273_511','1_380_004_385']]):
     print(f'{ca} is the capital of {co} , population equal {po} people')
+#Задача 2
+ignore_command=(lambda command : command in ['alias','configuration','ip','sql','select','updata','exec','del','truncate'])
+
+
